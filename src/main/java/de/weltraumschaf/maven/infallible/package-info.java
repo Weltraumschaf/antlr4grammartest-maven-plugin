5e010b401e9b01142e1a06dc0375114952c04937
Sven Strittmatter <sven.strittmatter@iteratec.de>
2016-01-13 00:33:37 +0100

    Add package info.

---
+++ src/main/java/de/weltraumschaf/maven/infallible/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * This package contains the whole plugin.
+ * <p>
+ * The only public API is {@link InfallibleMojo}.
+ * </p>
+ *
+ * @since 1.0.0
+ * @author Sven Strittmatter &lt;weltraumschaf@googlemail.com&gt;
+ */
+package de.weltraumschaf.maven.infallible;
